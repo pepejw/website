e121f6e5aa09b1957cde96d279058ecff19fad82
Pepe Wightmore <pepejw@proton.me>
2024-08-13 18:01:36 +0100

    new website things pog web site

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <navbar-main
+    :pages="pages"
+    :active-page="activePage"
+    :nav-link-click="(index) => (activePage = index)"
+  ></navbar-main>
+
+  <page-viewer :page="pages[activePage]"></page-viewer>
+</template>
+
+<script>
+import NavbarMain from './components/NavbarMain.vue'
+import PageViewer from './components/PageViewer.vue'
+
+export default {
+  components: {
+    NavbarMain,
+    PageViewer
+  },
+  data() {
+    return {
+      activePage: 0,
+      pages: [
+        {
+          link: { text: 'home' },
+          pageTitle: 'homepage',
+          content: "welcome to pepe's website!"
+        },
+        {
+          link: { text: 'contact' },
+          pageTitle: 'contact me',
+          content: 'reach out to me at pepejw@proton.me!'
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ src/components/NavbarMain.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav>
+    <div>
+      <a href="#">pepe's website</a>
+      <ul>
+        <li v-for="(page, index) in pages" :key="index">
+          <a
+            :class="{ active: activepage == index }"
+            aria-current="page"
+            :title="`go to the ${page.link.text} page`"
+            @click.prevent="navLinkClick(index)"
+            >{{ page.link.text }}</a
+          >
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: ['pages', 'activePage', 'navLinkClick']
+}
+</script>
